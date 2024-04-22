df01233939106eac1222130f46dcaeb6c47920b8
raulgodii <raulgodii13@gmail.com>
2024-04-22 23:01:47 +0200

    metiendo plantilla

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,27 +5,77 @@ import { logoutRequest } from './../api/auth';
 function Navbar() {
     const { isAuthenticated, logoutContext, user } = useAuth();
     return (
-        <nav>
-            {
-                isAuthenticated ? (
-                    <>
-                        <h1>Welcome "{user.username}"</h1>
-                        <Link to="/">Home</Link>
-                        <Link to="/crear">Crear</Link>
-                        <Link to="/profile">Profile</Link>
-                        <Link to="/" onClick={() => { logoutContext() }}>Logout</Link>
-                    </>
-                ) : (
-                    <>
-                        <Link to="/">Home</Link>
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                    </>
-                )
-            }
 
+        <header>
+            <nav className="navbar navbar-expand-lg mini-header header-light bg-transparent header-reverse header-demo glass-effect" data-header-hover="light">
+                <div className="container-lg">
+                    <div className="col-auto me-auto ps-lg-0">
+                        <Link className="navbar-brand" to="/">
+                            <img src="images/logo-black-small.png" data-at2x="images/logo-black-small@2x.png" alt="" className="default-logo" />
+                            <img src="images/logo-black-small.png" data-at2x="images/logo-black-small@2x.png" alt="" className="alt-logo" />
+                            <img src="images/logo-black-small.png" data-at2x="images/logo-black-small@2x.png" alt="" className="mobile-logo" />
+                        </Link>
+                    </div>
+                    <div className="col-auto menu-order position-static">
+                        <button className="navbar-toggler float-start" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-line"></span>
+                            <span className="navbar-toggler-line"></span>
+                            <span className="navbar-toggler-line"></span>
+                            <span className="navbar-toggler-line"></span>
+                        </button>
+                        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                            <ul className="navbar-nav">
+                                {
+                                    isAuthenticated ? (
+                                        <>
+                                            <li className="nav-item ">
+                                                <Link className="nav-link" to="/crear">Crear</Link>
+                                            </li>
+                                            <li className="nav-item ">
+                                                <Link className="nav-link" to="/gestionar">Gestionar</Link>
+                                            </li>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <li className="nav-item ">
+                                                <Link className="nav-link" to="/">Home</Link>
+                                            </li>
+                                            <li className="nav-item ">
+                                                <Link className="nav-link" to="/login">Login</Link>
+                                            </li>
+                                            <li className="nav-item ">
+                                                <Link className="nav-link" to="/register">Register</Link>
+                                            </li>
+                                        </>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    {
+                        isAuthenticated ? (
+                            <>
+                                <div className="col-auto pe-lg-0">
+                                    <div className="header-icon">
+                                        <div className="header-user-icon icon">
+                                            <Link to="/profile"><i className="feather icon-feather-user"></i></Link>
+                                        </div>
+                                        <div className="header-logout-icon icon">
+                                            <Link to="/" onClick={() => { logoutContext() }}><i className="feather icon-feather-log-out"></i></Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            <>
 
-        </nav>
+                            </>
+                        )
+                    }
+
+                </div>
+            </nav>
+        </header>
     )
 }
 
--- frontend/src/pages/CrearPage.jsx
+++ frontend/src/pages/CrearPage.jsx
@@ -8,27 +8,29 @@ function CrearPage() {
 
     return (
         <>
-            <h1>Crear CV</h1>
+            <section id="home" className="full-screen bg-dark-gray ipad-top-space-margin position-relative md-h-600px sm-h-500px" data-parallax-background-ratio="0.5" style={{backgroundImage: "url('https://via.placeholder.com/1920x1080')"}}>
+                <h1>Crear CV</h1>
 
-            <div>
-                Paso {currentStepIndex + 1} de {steps.length}
-            </div>
-            {step}
-            <div>
-                {currentStepIndex > 0 && (
-                    <button type="button" onClick={back}>
-                        Back
-                    </button>
-                )}
-                {currentStepIndex + 1 == steps.length ? (
-                    <Link to="/gestionar">
-                        <button>Finalizar</button>
-                    </Link>
-                ) : (
-                    <button onClick={next}>Siguiente</button>
-                )
-                }
-            </div>
+                <div>
+                    Paso {currentStepIndex + 1} de {steps.length}
+                </div>
+                {step}
+                <div>
+                    {currentStepIndex > 0 && (
+                        <button type="button" onClick={back}>
+                            Back
+                        </button>
+                    )}
+                    {currentStepIndex + 1 == steps.length ? (
+                        <Link to="/gestionar">
+                            <button>Finalizar</button>
+                        </Link>
+                    ) : (
+                        <button onClick={next}>Siguiente</button>
+                    )
+                    }
+                </div>
+            </section>
         </>
     )
 }
