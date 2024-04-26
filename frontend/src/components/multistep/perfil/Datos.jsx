4dc3b48f135fc26be919e789e163f64882a68664
raulgodii <102313699+raulgodii@users.noreply.github.com>
2024-04-26 20:56:05 +0200

    cambiando estilos de crear

--- frontend/src/components/multistep/perfil/Datos.jsx
+++ frontend/src/components/multistep/perfil/Datos.jsx
@@ -20,27 +20,43 @@ function Datos() {
             ...data.perfil,
             ...newData.perfil
         };
-    
+
         const updatedData = {
             ...data,
             perfil: updatedPerfil
         };
-    
+
         updateData(updatedData);
     };
 
     return (
-        <form onChange={handleSubmit(onChange)}>
-            <h4>Datos Personales</h4>
-            <label>Nombre:</label>
-            <input type="text" {...register("perfil.nombre")} value={data.perfil.nombre}/>
-            <label>Subtitulo:</label>
-            <input type="text" {...register("perfil.subtitulo")} value={data.perfil.subtitulo}/>
-            <label>Descripcion:</label>
-            <input type="text" {...register("perfil.descripcion")} value={data.perfil.descripcion}/>
-            <label>Fecha nacimiento:</label>
-            <input type="date" {...register("perfil.fecha_nacimiento")} value={data.perfil.fecha_nacimiento}/>
-        </form>
+        // <form onChange={handleSubmit(onChange)}>
+        //     <h4>Datos Personales</h4>
+        //     <label>Nombre:</label>
+        //     <input type="text" {...register("perfil.nombre")} value={data.perfil.nombre}/>
+        //     <label>Subtitulo:</label>
+        //     <input type="text" {...register("perfil.subtitulo")} value={data.perfil.subtitulo}/>
+        //     <label>Descripcion:</label>
+        //     <input type="text" {...register("perfil.descripcion")} value={data.perfil.descripcion}/>
+        //     <label>Fecha nacimiento:</label>
+        //     <input type="date" {...register("perfil.fecha_nacimiento")} value={data.perfil.fecha_nacimiento}/>
+        // </form>
+        <div className="col-xl-10 col-lg-12">
+            <form onChange={handleSubmit(onChange)} className="row contact-form-style-02" data-anime="{ &quot;el&quot;: &quot;childs&quot;, &quot;translateY&quot;: [30, 0], &quot;opacity&quot;: [0,1], &quot;duration&quot;: 800, &quot;delay&quot;: 200, &quot;staggervalue&quot;: 300, &quot;easing&quot;: &quot;easeOutQuad&quot; }">
+                <div className="col-md-6 mb-30px">
+                    <input className="input-name border-radius-4px border-color-white box-shadow-double-large form-control" type="text" placeholder="Nombre" {...register("perfil.nombre")} value={data.perfil.nombre}/>
+                </div>
+                <div className="col-md-6 mb-30px">
+                    <input className="border-radius-4px border-color-white box-shadow-double-large form-control" type="email" placeholder="Subtitulo" {...register("perfil.subtitulo")} value={data.perfil.subtitulo}/>
+                </div>
+                <div className="col-md-6 mb-30px">
+                    <input className="form-control border-radius-4px border-color-white box-shadow-double-large" type="date" aria-label="date"  value={data.perfil.fecha_nacimiento}/>
+                </div>
+                <div className="col-md-6 mb-30px ">
+                    <input className="input-name border-radius-4px border-color-white box-shadow-double-large form-control" type="text" placeholder="Descripcion" {...register("perfil.descripcion")} value={data.perfil.descripcion}/>
+                </div>
+            </form>
+        </div>
     )
 }
 
--- frontend/src/pages/CrearPage.jsx
+++ frontend/src/pages/CrearPage.jsx
@@ -8,31 +8,55 @@ function CrearPage() {
 
     return (
         <>
-            <section id="home" className="full-screen bg-dark-gray ipad-top-space-margin position-relative md-h-600px sm-h-500px" data-parallax-background-ratio="0.5" style={{ backgroundImage: "url('https://via.placeholder.com/1920x1080')" }}>
-                <h1>Crear CV</h1>
+            <section className="position-relative bg-very-light-gray">
+                <div className="container">
+                    <div className="row">
+                        <div className="col text-center">
+                            <h2 className="fw-700 alt-font text-dark-gray ls-minus-2px">Crear CV</h2>
+                        </div>
 
-                <div class="col fw-600 text-dark-gray text-center pt-11 pb-11 border-bottom border-color-extra-medium-gray overflow-hidden">
-                    <h3 class="mb-0" data-anime='{"translateY": [-50, 0], "opacity": [0,1], "duration": 800, "staggervalue": 300, "easing": "easeOutQuad" }'>fadeInDown</h3>
-                </div>
+                    </div>
+                    <div className="row mb-2">
+                        <h6 className="col text-center">Paso {currentStepIndex + 1} de {steps.length}</h6>
+                    </div>
+                    <div className="row align-items-center justify-content-center position-relative z-index-1">
+                        {step}
+                        <div className="row">
+                            <div className="col-xl-6 col-md-7">
 
-                <div>
-                    Paso {currentStepIndex + 1} de {steps.length}
-                </div>
-                {step}
-                <div>
-                    {currentStepIndex > 0 && (
-                        <button type="button" onClick={back}>
-                            Back
-                        </button>
-                    )}
-                    {currentStepIndex + 1 == steps.length ? (
-                        <Link to="/gestionar">
-                            <button>Finalizar</button>
-                        </Link>
-                    ) : (
-                        <button onClick={next}>Siguiente</button>
-                    )
-                    }
+                                {currentStepIndex > 0 && (
+                                    <button onClick={back} className="btn btn-dark-gray btn-box-shadow btn-switch-text btn-large left-icon btn-round-edge submit text-transform-none" type="submit">
+                                        <span>
+                                            <span><i className="line-icon-Arrow-Back3"></i></span>
+                                            <span className="btn-double-text" data-text="Volver">Volver</span>
+                                        </span>
+                                    </button>
+                                )}
+
+                            </div>
+                            <div className="col-xl-6 col-md-5 text-center text-md-end sm-mt-20px">
+                                {currentStepIndex + 1 == steps.length ? (
+                                    <Link to="/gestionar">
+                                        <button className="btn btn-dark-gray btn-box-shadow btn-switch-text btn-large left-icon btn-round-edge submit text-transform-none" type="submit">
+                                            <span>
+                                                <span><i className="line-icon-Arrow-Forward2"></i></span>
+                                                <span className="btn-double-text" data-text="Siguiente">Finalizar</span>
+                                            </span>
+                                        </button>
+                                    </Link>
+                                ) : (
+                                    <button onClick={next} className="btn btn-dark-gray btn-box-shadow btn-switch-text btn-large left-icon btn-round-edge submit text-transform-none" type="submit">
+                                        <span>
+                                            <span><i className="line-icon-Arrow-Forward2"></i></span>
+                                            <span className="btn-double-text" data-text="Siguiente">Siguiente</span>
+                                        </span>
+                                    </button>
+                                )
+                                }
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
