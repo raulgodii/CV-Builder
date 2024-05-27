c9cc8ea766a5f07ec2dd956267bbaa903d4afec0
raulgodii <102313699+raulgodii@users.noreply.github.com>
2024-05-27 19:10:56 +0200

    httpOnly

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,6 +32,7 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             sameSite: process.env.NODE_ENV === 'production' ? 'none': '',
             secure: process.env.NODE_ENV === 'production',
+            httpOnly: process.env.NODE_ENV === 'production',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         res.json({
@@ -69,6 +70,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             sameSite: process.env.NODE_ENV === 'production' ? 'none': '',
             secure: process.env.NODE_ENV === 'production',
+            httpOnly: process.env.NODE_ENV === 'production',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         res.json({
@@ -107,6 +109,7 @@ export const loginGoogle = async (req, res) => {
         res.cookie('token', token, {
             sameSite: process.env.NODE_ENV === 'production' ? 'none': '',
             secure: process.env.NODE_ENV === 'production',
+            httpOnly: process.env.NODE_ENV === 'production',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         res.json({
