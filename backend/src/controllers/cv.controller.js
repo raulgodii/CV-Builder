d9dc48de97e004eb2594282795cc7ed8b59a0605
raulgodii <102313699+raulgodii@users.noreply.github.com>
2024-05-31 15:37:36 +0200

    build

--- backend/src/controllers/cv.controller.js
+++ backend/src/controllers/cv.controller.js
@@ -162,18 +162,19 @@ export const convertPdf = async (req, res) => {
       </html>
     `;
 
-    const browser = await puppeteer.launch({
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
-    });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
   try {
     const page = await browser.newPage();
